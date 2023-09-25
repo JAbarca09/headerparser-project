8f5f5a8d46d5cf018bf9d872de43b06f174ceb23
Jesse Abarca <JesseAbarca888@gmail.com>
2023-09-25 11:50:19 -0700

    Send headers and add error handling

--- requestHeaderController.js
+++ requestHeaderController.js
@@ -1,3 +1,20 @@
+/*
+Example Output
+{"ipaddress":"159.20.14.100","language":"en-US,en;q=0.5",
+"software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}
+*/
+
 exports.getHeaders = (req, res) => {
-    res.json({message: "Hello!"})
+  try {
+    const ipaddress = req.socket.remoteAddress;
+    const language = req.headers['accept-language'];
+    const software = req.headers['user-agent'];
+    res.status(200).json({
+      ipaddress,
+      language,
+      software,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 };
